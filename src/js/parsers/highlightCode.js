3a6b1e3268c3e414804a1db5e0b454c691cb6f77
HorizonShadow <joshleblanc94@gmail.com>
2015-10-22 12:33:21 -0300

    Fixes issue #2. Replace show more text link with own async function to highlight text

--- src/js/parsers/highlightCode.js
+++ src/js/parsers/highlightCode.js
@@ -1,10 +1,30 @@
 window.hljs.initHighlightingOnLoad();
 const highlightParser = node => {
     if (node.classList.contains('message')) {
-    	let pres = node.querySelectorAll('pre');
-    	[...pres].forEach(pre => {
+    	let pre = node.querySelector('pre');
+    	let moreLink = node.querySelector('.more-data');
+    	if(pre && moreLink) {
     		window.hljs.highlightBlock(pre);
-    	});
+    		moreLink = removeAllListeners(moreLink);
+    		moreLink.addEventListener('click', (e) => highlightFullText(e, pre));
+    	}
     }
 };
+
+const removeAllListeners = (el) => {
+	let clone = el.cloneNode(true);
+	el.parentNode.replaceChild(clone, el);
+	return clone;
+}
+
+const highlightFullText = (e, pre) => {
+	e.preventDefault();
+	let anchor = e.target;
+	fetch(e.target.href).then((e) => e.text().then(text => {
+		pre.innerHTML = text;
+		hljs.highlightBlock(pre);
+		anchor.parentNode.removeChild(anchor);
+	}));
+}
+
 watcher.addParser(highlightParser, '.user-container message');
