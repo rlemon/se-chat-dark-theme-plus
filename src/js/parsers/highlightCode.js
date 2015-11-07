941cd94dbdb9ae895cd595199fd197f81c0bd904
Joshua LeBlanc <JoshLeBlanc94@gmail.com>
2015-11-07 12:07:55 -0400

    fixes #2
    Added removeAllListeners function
    Added highlightFullText function
    Rewrite .more-data click handler with highlightFullText if partial is a pre and has a .more-data anchor.
    Prefixed watcher call with window

--- src/js/parsers/highlightCode.js
+++ src/js/parsers/highlightCode.js
@@ -1,10 +1,34 @@
 window.hljs.initHighlightingOnLoad();
+
+const removeAllListeners = (el) => {
+	let clone = el.cloneNode(true);
+	el.parentNode.replaceChild(clone, el);
+	return clone;
+}
+
+const highlightFullText = (e, pre) => {
+	e.preventDefault();
+	let anchor = e.target;
+	window.fetch(e.target.href).then((e) => e.text().then(text => {
+		pre.innerHTML = text;
+		window.hljs.highlightBlock(pre);
+		anchor.parentNode.removeChild(anchor);
+	}));
+}
+
 const highlightParser = node => {
     if (node.classList.contains('message')) {
-    	let pres = node.querySelectorAll('pre');
-    	[...pres].forEach(pre => {
-    		window.hljs.highlightBlock(pre);
-    	});
+    	let pre = node.querySelector('pre');
+    	let moreLink = node.querySelector('.more-data');
+      	
+		if(pre) {
+        	window.hljs.highlightBlock(pre);
+      	}
+    	if(moreLink && pre) {
+    		moreLink = removeAllListeners(moreLink);
+    		moreLink.addEventListener('click', (e) => highlightFullText(e, pre));
+    	}
     }
 };
-watcher.addParser(highlightParser, '.user-container .message');
+
+window.watcher.addParser(highlightParser, '.user-container message');
