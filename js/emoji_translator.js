e2dde4999842286e4f26646df19388c7cad6e9d1
bitttttten <git@aharding.co.uk>
2016-11-10 20:48:21 +0100

    emojis parsed on semi colon

--- js/emoji_translator.js
+++ js/emoji_translator.js
@@ -2,7 +2,7 @@
     "use strict";
     const input = document.getElementById('input');
     input.addEventListener('keyup', event => {
-        if( event.which !== 32 ) return;
+        if( [ 32, 186 ].indexOf(event.which) === -1 ) return;
         input.value = input.value.split(' ').map(word => {
         	const found = emojiData.find(item => item.triggers.includes(word));
         	if( found ) {
