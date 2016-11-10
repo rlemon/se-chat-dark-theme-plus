0f08e4a4d3c04068c04f3956b9cbe698124c9651
bitttttten <git@aharding.co.uk>
2016-11-10 22:23:10 +0100

    alright alright, using includes

--- js/emoji_translator.js
+++ js/emoji_translator.js
@@ -2,7 +2,8 @@
     "use strict";
     const input = document.getElementById('input');
     input.addEventListener('keyup', event => {
-        if( [ 32, 186 ].indexOf(event.which) === -1 ) return;
+        const isAnAllowedKey = [ 32, 186 ].includes(event.which);
+        if( !isAnAllowedKey ) return;
         input.value = input.value.split(' ').map(word => {
         	const found = emojiData.find(item => item.triggers.includes(word));
         	if( found ) {
